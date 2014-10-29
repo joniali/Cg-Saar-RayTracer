5392e761298e8a5842ab9ef748fb198e00e00dcc
Junaid Ali <joniali786@gmail.com>
2014-10-29 21:47:02 +0100

    ray added

---
+++ rt/ray.cpp
@@ -0,0 +1,19 @@
+#include <rt/ray.h>
+#include <core\point.h>
+
+
+
+namespace rt{
+
+	
+	Ray::Ray(const Point& o, const Vector& d){
+	
+	this->o=o;
+	this->d=d.normalize();
+	
+	}
+	Point Ray::getPoint(float distance) const
+	{
+		return Point(o.x + distance*d.x,o.y+distance*d.y,o.z+distance*d.z);
+	}
+}
