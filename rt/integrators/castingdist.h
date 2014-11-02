a5f73c2dcc7a4f862a72c029b375570c93561247
Junaid Ali <joniali786@gmail.com>
2014-11-02 19:36:21 +0100

    added assignment 2

---
+++ rt/integrators/castingdist.h
@@ -0,0 +1,20 @@
+#ifndef CG1RAYTRACER_INTEGRATORS_CASTINGDIST_HEADER
+#define CG1RAYTRACER_INTEGRATORS_CASTINGDIST_HEADER
+
+#include <rt/integrators/integrator.h>
+
+namespace rt {
+
+class World;
+class Ray;
+class RGBColor;
+
+class RayCastingDistIntegrator : public Integrator {
+public:
+    RayCastingDistIntegrator(World* world, const RGBColor& nearColor, float nearDist, const RGBColor& farColor, float farDist);
+    virtual RGBColor getRadiance(const Ray& ray) const;
+};
+
+}
+
+#endif
