85057979d5edee559139e4a12c4753b60613d552
faev999 <dmtesfav@hotmail.com>
2014-11-13 16:34:34 +0100

    its done

--- main/a_indexing.cpp
+++ main/a_indexing.cpp
@@ -34,10 +34,12 @@ void a_indexing( SDL_Surface* screen) {
     engine1.test_render2(img,screen);
     img.writePNG("a3-1.png");
 
-	/*PerspectiveCamera cam2(Point(16.065f, -12.506f, 1.771f), Point(-0.286f, -0.107f, 1.35f) - Point(16.065f, -12.506f, 1.771f), Vector(0, 0, 1), pi / 8, pi / 6);
-    Renderer engine2(&cam2, &integrator);
-    engine2.test_render2(img ,screen);
-    img.writePNG("a3-2.png");*/
+	Image img2(80, 60);
+	RayCastingIntegrator integrator2(&world);
+	PerspectiveCamera cam2(Point(-8.85f, -7.85f, 7.0f), Vector(1.0f, 1.0f, -0.6f), Vector(0, 0, 1), pi / 8, pi / 6);
+    Renderer engine2(&cam2, &integrator2);
+    engine2.test_render2(img2 ,screen);
+    img2.writePNG("a3-2.png");
 
 	system("pause");
 	SDL_Quit();
