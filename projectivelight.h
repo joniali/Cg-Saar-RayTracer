75dc43a05c7a4040a29dce8bbfd3693d08a14851
faev999 <dmtesfav@hotmail.com>
2014-11-20 17:53:33 +0100

    one

---
+++ projectivelight.h
@@ -0,0 +1,29 @@
+#ifndef CG1RAYTRACER_LIGHTS_PROJECTIVELIGHT_HEADER
+#define CG1RAYTRACER_LIGHTS_PROJECTIVELIGHT_HEADER
+
+#include <core/scalar.h>
+#include <core/vector.h>
+#include <rt/lights/light.h>
+#include <core/point.h>
+#include <core/color.h>
+
+
+namespace rt {
+
+	class ProjectiveLight : public Light {
+	public:
+		ProjectiveLight() {}
+		ProjectiveLight(const Point& position, const Vector& direction, float angle, float exp, const RGBColor& intensity);
+		virtual LightHit getLightHit(const Point& p) const;
+		virtual RGBColor getIntensity(const LightHit& irr) const;
+
+	private:
+		float sangle, sexp;
+
+	};
+
+
+}
+
+#endif
+
